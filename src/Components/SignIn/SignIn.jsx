929486a4ab5517aa210de05de6aa46eb604895bc
Suhrid-Talukder333 <suhridtalukder333@gmail.com>
2021-05-01 12:34:32 +0600

    finished sign in

--- src/Components/SignIn/SignIn.jsx
+++ src/Components/SignIn/SignIn.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import FormInput from "../FormInput/FormInput";
 import CustomButton from "../CustomButton/CustomButton";
 import "./styles.scss";
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 export default class SignIn extends Component {
   constructor(props) {
     super(props);
@@ -13,9 +13,15 @@ export default class SignIn extends Component {
     };
   }
 
-  handleSubmit = (e) => {
+  handleSubmit = async (e) => {
     e.preventDefault();
-    this.setState({ email: "", password: "" });
+    const { email, password } = this.state;
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: "", password: "" });
+    } catch (error) {
+      console.log("Error ", error.message);
+    }
   };
 
   handleChange = (e) => {
